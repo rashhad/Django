d4d78a3e47acfc51c95078e3ad2df75975485aab
Md. Rashadul Islam <119654652+rashhad@users.noreply.github.com>
2023-09-20 12:31:48 +0600

    Delete django_assignment_01 directory

--- django_assignment_01/app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def platter(request):
-    # return HttpResponse('thist is platter page')
-    return render(request, './app/index.html')
